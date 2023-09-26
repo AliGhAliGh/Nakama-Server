c2c5a15b186ca44980c9b86f7b1ed9e02d41de5f
AliGhAliGh <73810908+AliGhAliGh@users.noreply.github.com>
2023-09-26 22:12:23 +0330

    set postgres and correct rpc

---
+++ main.ts
@@ -0,0 +1,4 @@
+function InitModule(ctx: nkruntime.Context, logger: nkruntime.Logger, nk: nkruntime.Nakama, initializer: nkruntime.Initializer) {
+    initializer.registerRpc("HealthCheck", rpcHealthcheck);
+    logger.info('javascript has loaded!');
+}
