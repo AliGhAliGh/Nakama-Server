94314bfcf72ead01debc4ee98620f06a33eb2473
AliGhAliGh <73810908+AliGhAliGh@users.noreply.github.com>
2023-09-30 20:25:30 +0330

    static data added

--- healthcheck.ts
+++ healthcheck.ts
@@ -1,4 +1,4 @@
 function rpcHealthcheck(ctx: nkruntime.Context, logger: nkruntime.Logger, nk: nkruntime.Nakama, payLoad: string): string {
-    logger.info('healthcheck rpc called!');
-    return JSON.stringify({ success: true });
+    logger.info(payLoad + 'healthcheck rpc called!');
+    return JSON.stringify({ success: true, payLoad: payLoad });
 }
