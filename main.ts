f442c9b40f90f42bc3b3518978f7871c00f77f04
AliGhAliGh <73810908+AliGhAliGh@users.noreply.github.com>
2023-10-15 06:20:03 +0330

    add a test before hook

--- main.ts
+++ main.ts
@@ -1,4 +1,18 @@
 function InitModule(ctx: nkruntime.Context, logger: nkruntime.Logger, nk: nkruntime.Nakama, initializer: nkruntime.Initializer) {
     initializer.registerRpc("HealthCheck", rpcHealthcheck);
+    initializer.registerRtAfter("MatchCreate", MatchCreateBeforeHook);
     logger.info('javascript has loaded!');
 }
+
+const MatchCreateBeforeHook: nkruntime.RtAfterHookFunction<nkruntime.Envelope> = (ctx: nkruntime.Context, logger: nkruntime.Logger, nk: nkruntime.Nakama, output: nkruntime.Envelope | null, input: nkruntime.Envelope): void => {
+    nk.storageWrite([{
+        key: '',
+        collection: '',
+        userId: '',
+        permissionRead: 1,
+        permissionWrite: 0,
+        value: {
+            LastMatch: ctx.
+        }
+    }])
+};
