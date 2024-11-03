aa74485c7a6d72ab1ddb9e90fe0ef6687287f58e
AliGhAliGh <73810908+AliGhAliGh@users.noreply.github.com>
2024-11-03 19:24:15 +0330

    version 1

---
+++ install/downloader.ps1
@@ -0,0 +1,76 @@
+﻿param (
+    [string]$cockroachUrl,
+    [string]$resultDir,
+    [string]$cockroachZip,
+    [string]$cockroachExe,
+    [string]$cockroachExtractDir,
+    [string]$nakamaUrl,
+    [string]$nakamaTar,
+    [string]$nakamaExe
+)
+
+function DownloadFile {
+    param (
+        [string]$url,
+        [string]$outputPath
+    )
+
+    $webClient = New-Object System.Net.WebClient
+    $response = $webClient.OpenRead($url)
+    $totalLength = [int]$webClient.ResponseHeaders['Content-Length']
+    $readBytes = 0
+    $buffer = New-Object byte[] 4096
+    $fileStream = [System.IO.File]::Create($outputPath)
+
+    while (($read = $response.Read($buffer, 0, $buffer.Length)) -gt 0) {
+        $fileStream.Write($buffer, 0, $read)
+        $readBytes += $read
+        $percent = [math]::Round(($readBytes / $totalLength) * 100, 2)
+        
+        $progressBar = ('#' * ($percent / 2)) + ('-' * (50 - ($percent / 2)))
+        Write-Host -NoNewline "`r[$progressBar] $percent%"
+
+        Start-Sleep -Milliseconds 100 
+    }
+
+    $fileStream.Close()
+    $response.Close()
+    Write-Host  
+}
+
+if (-not (Test-Path -Path $nakamaExe)) {
+    Write-Output "Nakama executable not found, downloading..."
+    if (-not (Test-Path -Path $resultDir)) { New-Item -ItemType Directory -Path $resultDir }
+
+    DownloadFile -url $nakamaUrl -outputPath $nakamaTar
+
+    tar -xf $nakamaTar -C $resultDir
+    
+    Get-ChildItem -Path $resultDir -File | Where-Object { $_.Name -ne "nakama.exe" } | Remove-Item -Force
+}
+else {
+    Write-Output "Nakama executable already exists, skipping download."
+}
+
+if (-not (Test-Path -Path $cockroachExe)) {
+    Write-Output "Cockroach executable not found, downloading..."
+    if (-not (Test-Path -Path $resultDir)) { New-Item -ItemType Directory -Path $resultDir }
+
+    DownloadFile -url $cockroachUrl -outputPath $cockroachZip
+
+    Expand-Archive -Path $cockroachZip -DestinationPath $resultDir -Force
+    Remove-Item -Path $cockroachZip -Force
+
+    if (Test-Path "$cockroachExtractDir\cockroach.exe") {
+        Move-Item -Path "$cockroachExtractDir\cockroach.exe" -Destination $resultDir
+        Remove-Item -Recurse -Force -Path $cockroachExtractDir
+    }
+    else {
+        Write-Output "cockroach.exe not found in extracted folder."
+    }
+}
+else {
+    Write-Output "Cockroach executable already exists, skipping download."
+}
+
+Write-Output "`nProcess completed."
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,16 @@
 function InitModule(ctx: nkruntime.Context, logger: nkruntime.Logger, nk: nkruntime.Nakama, initializer: nkruntime.Initializer) {
-    initializer.registerRpc("HealthCheck", rpcHealthcheck);
     initializer.registerRpc("ChangeMetaData", UpdateMetadata);
     initializer.registerRpc("ChangeWallet", UpdateWallet);
     initializer.registerRpc("CreateMatch", CreateMatch);
     initializer.registerRpc("MatchList", MatchList);
     initializer.registerMatch("main", {
-        matchInit,
-        matchJoin,
-        matchLeave,
-        matchLoop,
-        matchJoinAttempt,
-        matchSignal,
-        matchTerminate
-    })
+        matchInit: matchInit,
+        matchJoin: matchJoin,
+        matchLeave: matchLeave,
+        matchLoop: matchLoop,
+        matchJoinAttempt: matchJoinAttempt,
+        matchSignal: matchSignal,
+        matchTerminate: matchTerminate
+    });
     logger.info('javascript has loaded v2!');
 }
